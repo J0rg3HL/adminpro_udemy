b1d9f37cc22391430884b8be0048bf0c709c2f35
J0rg3HL <hernandezloj@gmail.com>
2018-12-29 19:59:10 -0600

    modificacion en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { NgModule } from '@angular/core';
 // rutas
 import { APP__MAIN_ROUTES } from './app.routes';
 
+// modulos
+
+import { PagesModule } from './pages/pages.module';
+
 
 // componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProgressComponent } from './pages/progress/progress.component';
@@ -17,25 +22,26 @@ import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
+    // NopagefoundComponent
+    // DashboardComponent,
+    // ProgressComponent,
+    // Graficas1Component,
+    // PagesComponent,
+
+    // HeaderComponent,
+    // SidebarComponent,
+    // BreadcrumbsComponent,
   ],
   imports: [
     BrowserModule,
-    APP__MAIN_ROUTES
+    APP__MAIN_ROUTES,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
